8dffa325ebdaca409fbb399515e28a60a8f1a5ad
Christian Mulato <chmulato@gmail.com>
2018-01-27 16:36:18 -0200

    Este é o código final

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,39 +1,11 @@
 import { Component } from '@angular/core';
+import { PostsService } from '../services/posts.service';
 
 @Component({
-  selector: 'user',
-  template: `
-    <h1>Name: {{name}}</h1>
-    <p><strong>Email:</strong> {{email}}</p>
-    <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
-    <button (click)="toggleHobbies()">{{showHobbies ? 'Hide Hobbies' : 'Show Hobbies'}}</button>
-    <div *ngIf="showHobbies">
-        <h3>Hobbies</h3>
-        <ul>
-            <li *ngFor="let hobby of hobbies; let i = index">
-                {{hobby}} <button (click)="deleteHobby(i)">X</button>
-            </li>
-        </ul>
-        <form (submit)="addHobby(hobby.value)">
-            <label>Add Hobby: </label> <br />
-            <input type="text" #hobby /> <br />
-        </form>
-    </div>
-    <hr />
-    <h3>Edit User</h3>
-    <form>
-        <label>Name: </label> <br />
-        <input type="text" name="name" [(ngModel)]="name" /> <br />
-        <label>Email: </label> <br />
-        <input type="text" name="email" [(ngModel)]="email" /> <br />
-        <label>Street: </label> <br />
-        <input type="text" name="address.street" [(ngModel)]="address.street" /> <br />
-        <label>City: </label> <br />
-        <input type="text" name="address.city" [(ngModel)]="address.city" /> <br />
-        <label>State: </label> <br />
-        <input type="text" name="address.state" [(ngModel)]="address.state" />
-    </form>
-    `,
+    moduleId: module.id,
+    selector: 'user',
+    templateUrl: 'user.component.html',
+    providers: [ PostsService ]
 })
 export class UserComponent  { 
   name: string; 
@@ -41,8 +13,9 @@ export class UserComponent  {
   address: address;
   hobbies: string[];
   showHobbies: boolean;
+  posts: Post[];
 
-  constructor() {
+  constructor(private postsService: PostsService){
       this.name = 'Christian Vladimir Uhdre Mulato';
       this.email = 'chmulato@hotmail.com';
       this.address = {
@@ -53,6 +26,10 @@ export class UserComponent  {
       this.hobbies = ['Musics','Movies','Reading Books'];
       this.showHobbies = false;
       //console.log('constructor ran');
+
+      this.postsService.getPosts().subscribe(posts => {
+          this.posts = posts;
+      })
   }
 
   toggleHobbies() {
@@ -72,7 +49,7 @@ export class UserComponent  {
   deleteHobby(i: number) {
     this.hobbies.splice(i, 1);
   }
-  
+
 }
 
 // this is a interface declaration type
@@ -80,4 +57,10 @@ interface address {
     street: string,
     city: string,
     state: string
+}
+
+interface Post {
+    id: number,
+    title: string,
+    body: string
 }
